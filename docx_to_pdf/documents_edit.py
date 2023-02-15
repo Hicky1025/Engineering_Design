e83c12f1d7e9028af5a8a5554881a6f404d579fc
kanata-1108 <kanatako08@yahoo.co.jp>
2023-02-15 12:57:48 +0900

    20230215

--- docx_to_pdf/documents_edit.py
+++ docx_to_pdf/documents_edit.py
@@ -4,25 +4,15 @@ from docx import Document
 # .docxを読み込んでDocumentオブジェクトの作成
 doc = Document("./sample.docx")
 
-class Docs:
-    def __init__(self, number, year, month, day, address, group, address_name):
-        self.number = number
-        self.year = year
-        self.month = month
-        self.day = day
-        self.address = address
-        self.group = group
-        self.address_name = address_name
-
 # 段落を取得して、その段落に書かれているテキストを抽出・出力
-# for num, paragraph in enumerate(doc.paragraphs):
-#     print(num, paragraph.text)
+for num, paragraph in enumerate(doc.paragraphs):
+    print(num, paragraph.text)
 
-number = input()
-Y, M, D = input().split()
-address = input()
-group = input()
-address_name = input()
+# number = input()
+# Y, M, D = input().split()
+# address = input()
+# group = input()
+# address_name = input()
 
 # print(doc.paragraphs)
 # dates = doc.paragraphs[2]
